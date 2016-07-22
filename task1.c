b8bc4c4adc6c9c9e75a0f08805e78855973bb014
tititititotototo <grooves@naver.com>
2016-07-22 18:26:56 +0900

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,37 @@
+//task1
+
+#include <uart.h>
+#include <system.h>
+
+int main(void) {
+
+	// while(1)
+	// {
+	// 	local_irq_disable();
+	// 	uart_putc('1');
+	// 	local_irq_enable();
+	// 	//schedule();
+	// }
+
+    #define SUM *(volatile unsigned int*)0x200000
+
+    static unsigned int tmp_sum;
+    static unsigned int count = 0;
+
+	while(1) {
+		tmp_sum = SUM;	
+		if(tmp_sum >= 0x100000) break;		
+		tmp_sum++;
+		SUM = tmp_sum;
+		count++;
+	}
+
+	local_irq_disable();
+	uart_puts("sum in task1 ");
+	uart_puthexnl(SUM);
+	uart_puts("count in task1 ");
+	uart_puthexnl(count);
+	local_irq_enable();
+
+	while(1);
+}
